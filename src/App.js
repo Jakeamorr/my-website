eac07406ab44a631aab996fcae5e440a88f6c85d
Jake Morris <Jakeamorr@gmail.com>
2021-01-26 13:33:46 -0500

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
+import Home from './pages/home.js';
+import About from './pages/about.js';
+import Projects from './pages/projects.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Router>
+                    <div className="Nav-bar">
+                        {/* Configure Navbar */}
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/about">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/projects">Projects</Link>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        {/* Connect routes to corresponding component */}
+                        <Switch>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/projects">
+                                <Projects />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
+            </header>
+        </div>
+    );
 }
 
 export default App;
